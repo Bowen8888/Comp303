9628b341755b9f01b8f748e4f4239eb9393fccad
Bowen Tian <bowen.tian@mail.mcgill.ca>
2017-11-01 19:57:43 -0400

    PracticeMidterm2016Q4added

---
+++ src/PracticeMidtermQ4/Assembly.java
@@ -0,0 +1,24 @@
+package PracticeMidtermQ4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Assembly implements Part
+{
+	private List<Part> aParts = new ArrayList<>();
+	
+	public Assembly(Part... pParts)
+	{
+		for(Part part: pParts)
+		{
+			aParts.add(part);
+		}
+	}
+	
+	@Override
+	public void move(int x, int y) {
+		
+		
+	}
+
+}
---
+++ src/PracticeMidtermQ4/Part.java
@@ -0,0 +1,6 @@
+package PracticeMidtermQ4;
+
+public interface Part 
+{
+	public void move(int x, int y);
+}
