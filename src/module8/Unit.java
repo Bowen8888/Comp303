bfc7e1b3d9aa7e90f20457468da85f32f47ac56f
Bobo <bowen.tian@mail.mcgill.ca>
2017-11-19 16:20:38 -0500

    Visitor design pattern added

---
+++ src/module8/Unit.java
@@ -0,0 +1,7 @@
+package module8;
+
+public interface Unit extends Iterable<Unit>, Visitable
+{
+	String name();
+	void print(String pPrefix);
+}
---
+++ src/module8/Visitable.java
@@ -0,0 +1,6 @@
+package module8;
+
+public interface Visitable
+{
+	void accept(UnitVisitor pVisitor);
+}
