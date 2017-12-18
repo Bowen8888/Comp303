c09dbac1ca45dc6a6494df2abc00107c77f22b90
btian <btian@520-1B16-10.campus.MCGILL.CA>
2017-12-18 10:49:20 -0500

    Overload example

--- src/Inheritance/Overloader.java
+++ src/Inheritance/Overloader.java
@@ -3,13 +3,13 @@ package Inheritance;
 public class Overloader {
 	public static void main(String[] pArgs)
 	{
-		System.out.println(new Overloader().doit("Foo"));
-		System.out.println(new Overloader().doit(12));
-		System.out.println(new Overloader().doit(12.3));
-		System.out.println(new Overloader().doit(new Manager("Alice",12000,2000)));
-		System.out.println(new Overloader().doit(new double[10]));
-		System.out.println(new Overloader().doit(Long.MAX_VALUE));
-		System.out.println(new Overloader().doit(new Employee("AMO",1200)));
+		System.out.println("1 " + new Overloader().doit("Foo"));
+		System.out.println("2 " +new Overloader().doit(12));
+		System.out.println("3 " +new Overloader().doit(12.3));
+		System.out.println("4 " +new Overloader().doit(new Manager("Alice",12000,2000)));
+		System.out.println("5 " +new Overloader().doit(new double[10]));
+		System.out.println("6 " +new Overloader().doit(Long.MAX_VALUE));
+		System.out.println("7 " +new Overloader().doit(new Employee("AMO",1200)));
 		
 		String foo = "Foo";
 		new Overloader().redirector(foo);
@@ -19,7 +19,10 @@ public class Overloader {
 		
 		double x = 100 ;
 		int y = (int) x;
-		System.out.println(new Overloader().doit(y));
+		double z = y;
+		System.out.println("8 " +new Overloader().doit(y));
+
+		System.out.println("9 " +new Overloader().doit(z));
 	}
 	
 	public void redirector(Object p){
